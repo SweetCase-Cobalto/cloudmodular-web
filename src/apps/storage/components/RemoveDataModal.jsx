2907c7b2a60c70b81a9b4cdeb59d54dd641d440a
Vector-7 <seokbong60@gmail.com>
2022-07-25 19:27:19 +0900

    add: remove file/directory

--- src/apps/storage/components/RemoveDataModal.jsx
+++ src/apps/storage/components/RemoveDataModal.jsx
@@ -1,23 +1,19 @@
 import styled from "styled-components";
-import { Modal } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { Modal, ModalBody, ProgressBar } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import { AccessedButton, CanceledButton, DangerOutlinedButton } from "../../../components/common/Buttons";
+import { removeData } from "../../../util/apis";
 
 const RemoveDataModal = (props) => {
     const currentDir = useSelector(state => state.storageResult);
-    const idxs = currentDir.selectedIdxs;
-    const dataList = currentDir.dataList;
-
-    const removeEvent = () => {
-        // 데이터 삭제
-        idxs.forEach((dataIdx) => {
-            // 순회하면서 삭제
-            const removedId = dataList[dataIdx].id;
-        });
-        // Reload
-        window.location.reload();
-    }
-
+    const [searchParams, ] = useSearchParams(); // Query Params
+    const [cookie, ,] = useCookies(['token', 'user_id']);   // 쿠키
+    const idxs = currentDir.selectedIdxs;   // 삭제 대상의 리스트 인덱스들
+    const dataList = currentDir.dataList;   // 데이터 정보 리스트: 여기에서 ID값을 빼내옴
+    const [removeList, setRemoveList] = useState(undefined);
 
     if(idxs.size < 1) {
         // 삭제 대상의 어떤 데이터도 선택되지 않음
@@ -38,33 +34,90 @@ const RemoveDataModal = (props) => {
                 </Modal.Footer>
             </Modal>
         );
+    } else if(removeList !== undefined && removeList.length > 0) {
+        // 삭제 진행 중
+        const percentage    = ((idxs.size - removeList.length) / idxs.size) * 100;
+        const removedIdx    = removeList[0];
+        const removeId      = dataList[removedIdx].id;
+        // 서버에서 데이터 삭제
+        // removeList에서 데이터 하나 제거
+        removeData(cookie.token, cookie.user_id, removeId).finally(() => {
+            // 성공 여부 상관없이 removeList에서 데이터 제거
+            setRemoveList(removeList.filter(e => e !== removedIdx));
+        });
+
+        return (
+            <Modal
+                {...props}
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header>
+                    <Modal.Title>데이터 삭제</Modal.Title>
+                </Modal.Header>
+                <ModalBody>
+                    <p>삭제 중</p>
+                    <ProgressBar variant="success" now={percentage} />
+                </ModalBody>
+            </Modal>
+        );
+    } else if(removeList !== undefined && removeList.length === 0) {
+        // 삭제 왼료 창
+        // idxs 초기화
+        const onHideEvent = () => {
+            // Reload Browser
+            let rootId = searchParams.get('id');   // 검색 대상의 디렉토리 아이디
+            window.location.replace(`/storage?id=${rootId}`);
+        }
+        return (
+            <Modal
+                {...props}
+                aria-labelledby="contained-modal-title-vcenter"
+                onHide={onHideEvent}
+                centered
+            >
+                <Modal.Header>
+                    <Modal.Title>데이터 삭제</Modal.Title>
+                </Modal.Header>
+                <ModalBody>
+                    <p>삭제가 완료 되었습니다.</p>
+                </ModalBody>
+                <Modal.Footer>
+                    <AccessedButton onClick={onHideEvent}>확인</AccessedButton>
+                </Modal.Footer>
+            </Modal>
+        )
+    } else {
+        // 삭제 여부 묻는 데이터
+        // 삭제 데이터 명단 작성
+        const RemovedListElements = [];
+        idxs.forEach((dataIdx, i, ) => {
+            RemovedListElements.push(
+                <p key={i}>{dataList[dataIdx].name}</p>
+            );
+        });
+        return (
+            <Modal
+                {...props}
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>데이터 삭제</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <RemovedListForm>
+                        {RemovedListElements}
+                    </RemovedListForm>
+                    <p>정말 삭제하시겠습니까?</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <DangerOutlinedButton onClick={() => setRemoveList(Array.from(idxs))}>삭제</DangerOutlinedButton>
+                    <CanceledButton onClick={props.onHide}>취소</CanceledButton>
+                </Modal.Footer>
+            </Modal>
+        )
     }
-    // 삭제 데이터 명단 작성
-    const RemovedList = [];
-    idxs.forEach((dataIdx, i, ) => {
-        RemovedList.push(<p key={i}>{dataList[dataIdx].name}</p>);
-    })
-    return (
-        <Modal
-            {...props}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>데이터 삭제</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <RemovedListForm>
-                    {RemovedList}
-                </RemovedListForm>
-                <p>정말 삭제하시겠습니까?</p>
-            </Modal.Body>
-            <Modal.Footer>
-                <DangerOutlinedButton onClick={removeEvent}>삭제</DangerOutlinedButton>
-                <CanceledButton onClick={props.onHide}>취소</CanceledButton>
-            </Modal.Footer>
-        </Modal>
-    )
 }
 export default RemoveDataModal;
 
